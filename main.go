5086044ad09bc1b42080eebc0bf63ebb70864393
adham90 <adham.eldeeb90@gmail.com>
2018-02-11 18:08:16 +0200

    init commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	fmt.Print("phone num normalizer")
+}
+
+func normalize(phone string) string {
+	var buf bytes.Buffer
+
+	for _, ch := range phone {
+		if ch >= '0' && ch <= '9' {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return buf.String()
+}
